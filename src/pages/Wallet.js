6bd11138a9e969f9c5451742a158c94728801ca2
Guilherme Leao <leaob7@gmail.com>
2021-10-18 17:01:14 -0300

    req 4 and 6

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,16 +1,54 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
   render() {
     const { email } = this.props;
 
     return (
-      <header>
-        <p data-testid="email-field">{email}</p>
-        <p data-testid="total-field">{0}</p>
-        <p data-testid="header-currency-field">BRL</p>
-      </header>
+      <>
+        <header>
+          <p data-testid="email-field">{email}</p>
+          <p data-testid="total-field">{0}</p>
+          <p data-testid="header-currency-field">BRL</p>
+        </header>
+        <form>
+          <label htmlFor="value">
+            Valor
+            <input type="text" id="value" />
+          </label>
+          <label htmlFor="descricao">
+            Descrição
+            <input type="text" id="descricao" />
+          </label>
+          <label htmlFor="moeda">
+            Moeda
+            <select id="moeda" >
+
+            </select>
+          </label>
+          <label htmlFor="pagamento">
+            Método de pagamento
+            <select id="pagamento">
+              <option>Dinheiro</option>
+              <option>Cartão de crédito</option>
+              <option>Cartão de débito</option>
+            </select>
+          </label>
+          <label htmlFor="tag">
+            Tag
+            <select id="tag">
+              <option>Alimentação</option>
+              <option>Lazer</option>
+              <option>Trabalho</option>
+              <option>Transporte</option>
+              <option>Saúde</option>
+            </select>
+          </label>
+
+        </form>
+      </>
     );
   }
 }
@@ -18,6 +56,10 @@ class Wallet extends React.Component {
 const mapStateToProps = (state) => ({
   email: state.user.email,
 });
+
+Wallet.propTypes = {
+  email: PropTypes.string.isRequired,
+};
 
 export default connect(mapStateToProps, null)(Wallet);
 // export default Wallet;
