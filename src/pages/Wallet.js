e71758f5142568d975913caddf9406bd7d8647d0
Guilherme Leao <leaob7@gmail.com>
2021-10-18 17:44:08 -0300

    req 7 fetch money from API

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -3,9 +3,46 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 class Wallet extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      dataCoins: [],
+    };
+    this.fecthDataCoins = this.fecthDataCoins.bind(this);
+  }
+
+  componentDidMount() {
+    this.fecthDataCoins();
+  }
+
+  async fecthDataCoins() {
+    const requisition = await fetch('https://economia.awesomeapi.com.br/json/all');
+    const data = await requisition.json();
+    delete data.USDT;
+    this.setState({
+      dataCoins: data,
+    });
+  }
+
+  tagSelectOptions() {
+    return (
+      <label htmlFor="tag">
+        Tag
+        <select id="tag">
+          <option>Alimentação</option>
+          <option>Lazer</option>
+          <option>Trabalho</option>
+          <option>Transporte</option>
+          <option>Saúde</option>
+        </select>
+      </label>
+    );
+  }
+
   render() {
     const { email } = this.props;
-
+    const { dataCoins } = this.state;
+    const dataCoinsVet = Object.keys(dataCoins);
     return (
       <>
         <header>
@@ -24,8 +61,12 @@ class Wallet extends React.Component {
           </label>
           <label htmlFor="moeda">
             Moeda
-            <select id="moeda" >
-
+            <select id="moeda">
+              { dataCoinsVet.map((moeda, index) => (
+                <option key={ index }>
+                  {moeda === 'USDT' ? null : moeda }
+                </option>
+              )) }
             </select>
           </label>
           <label htmlFor="pagamento">
@@ -36,17 +77,7 @@ class Wallet extends React.Component {
               <option>Cartão de débito</option>
             </select>
           </label>
-          <label htmlFor="tag">
-            Tag
-            <select id="tag">
-              <option>Alimentação</option>
-              <option>Lazer</option>
-              <option>Trabalho</option>
-              <option>Transporte</option>
-              <option>Saúde</option>
-            </select>
-          </label>
-
+          { this.tagSelectOptions() }
         </form>
       </>
     );
