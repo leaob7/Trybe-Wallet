cc93ea6b4ea200c0b5bc6185c8117ce840da6e40
Guilherme Leao <leaob7@gmail.com>
2022-03-31 10:43:39 -0300

    last changes for using project

--- src/utils/htFunctions.js
+++ src/utils/htFunctions.js
@@ -1,6 +1,3 @@
-// import React from 'react';
-
-// https://thispointer.com/javascript-remove-everything-after-a-certain-character/
 export default function toBefSubstring(string) {
   const str = string.split('/')[0];
   return str;
